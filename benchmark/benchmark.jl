c502e632378f223c4d5c001b43efcf81bc7daf46
Ashwani Rathee <ab669522@gmail.com>
2021-05-15 20:55:24 +0530

    Initial commit for benchmarks

---
+++ benchmark/benchmark.jl
@@ -0,0 +1,67 @@
+using BenchmarkTools
+using ImageDistances
+using TestImages
+
+const SUITE = BenchmarkGroup()
+
+function create_distances()
+    dists = [
+        SqEuclidean(),
+        Euclidean(),
+        Cityblock(),
+    ]
+    return dists
+end
+
+###########
+# Colwise #
+###########
+
+SUITE["colwise"] = BenchmarkGroup()
+
+function add_colwise_benchmarks!(SUITE)
+
+    imgA = testimage("cameraman") # N0f8
+    imgB = testimage("lena_gray_512") # N0f8
+
+    # two lists of images
+    imgsA = [imgA, imgB, imgA, imgB]
+    imgsB = [imgB, imgA, imgA, imgB]
+
+    dists = create_distances()
+
+    for dist in (dists)
+        Tdist = typeof(dist)
+        SUITE["colwise"][Tdist] = BenchmarkGroup()
+        SUITE["colwise"][Tdist]["specialized"] = @benchmarkable colwise($dist, $imgsA, $imgsB)
+    end
+end
+
+add_colwise_benchmarks!(SUITE)
+
+
+############
+# Pairwise #
+############
+
+SUITE["pairwise"] = BenchmarkGroup()
+
+function add_pairwise_benchmarks!(SUITE)
+
+    imgA = testimage("cameraman") # N0f8
+    imgB = testimage("lena_gray_512") # N0f8
+
+    # two lists of images
+    imgsA = [imgA, imgB, imgA, imgB]
+    imgsB = [imgB, imgA, imgA, imgB]
+
+    dists = create_distances()
+
+    for dist in (dists)
+        Tdist = typeof(dist)
+        SUITE["pairwise"][Tdist] = BenchmarkGroup()
+        SUITE["pairwise"][Tdist]["specialized"] = @benchmarkable pairwise($dist, $imgsA, $imgsB)
+    end
+end
+
+add_pairwise_benchmarks!(SUITE)
